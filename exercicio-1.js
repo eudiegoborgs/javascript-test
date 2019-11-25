acc641013f075555cfe83c454590c3bfd699e426
Diego Borges <diego.ferreira@maxmilhas.com.br>
2019-11-25 14:48:10 -0300

    First commit

---
+++ exercicio-1.js
@@ -0,0 +1,65 @@
+/** 
+
+Exercício 1
+Faça um programa que leia 2 números, some - os e exiba uma mensagem com o resultado comparando se os valores são maiores, menores ou iguais a 100.
+*/
+
+// Como pedir ao usuario que insira algo no prompt
+var um = parseInt(window.prompt('Digite o primeiro valor:', 'Digite aqui!'));
+
+// Continue aqui
+
+/*
+Exercício 2
+Escreva um programa que calcule e mostre a tabuada (até 10x) de um número qualquer, digitado pelo usuário.
+*/
+
+/*
+Exercício 3
+Faça um programa que leia um vetor numérico de 10 posições. Depois, ordene o vetor em ordem crescente e apresente os elementos ordenados.
+*/
+
+/*
+Exercício 4
+Escreva um programa que leia dois números e apresente um menu de opções como o mostrado abaixo:
+
+Escolha a opção: A- Soma de 2 números. B- Diferença entre 2 números (maior pelo menor). C- Produto entre 2 números. D- Divisão entre 2 números (o denominador não pode ser zero).
+
+Leia a opção do usuário e execute a operação com os dois números lidos anteriormente. Caso a opção digitada seja inválida, apresente a mensagem de “Opção inválida” para o usuário. Lembre-se de usar funções para organizar seu programa.
+*/
+
+/*
+Exercício 5
+Uma eleição possui 2 candidatos a prefeito: o candidato José, do Partido da Felicidade e o candidato João, do Partido da Esperança. Os eleitores votam nos candidatos pelo número do partido. Para votar no candidato José do Partido da Felicidade, precisam digitar o número 1. Para votar no candidato João, do Partido da Esperança, precisam digitar o número 2.
+
+Faça um programa que simule uma urna eletrônica. Para cada eleitor, deve ser lido o número do seu título e em seguida o eleitor poderá digitar o seu voto. A eleição é encerrada quando um valor negativo (menor do que 0) para o título do eleitor é digitado. Informe quantos votos cada candidato teve e qual deles ganhou a eleição.
+*/
+
+/*
+Exercício 6
+Faça um programa que peça ao usuário uma temperatura em Fahrenheit, converta-a para graus Celsius e escreva o novo valor na tela. A fórmula de conversão de Fahrenheit (F) para Celsius é C = ( ( F - 32 ) * 5 ) / 9. Exemplo: 100 Fahrenheit = 37,77 Celsius.
+*/
+
+/*
+Exercício 7
+Para um jogo, precisamos saber se um personagem morreu ou não depois de sofrer um ataque. Faça uma função que receba 2 parâmetros, DANO e SAUDE. A função deve retornar verdadeiro se o dano for matar o personagem (ou seja, deixar sua saúde menor ou igual a zero) e falso caso contrário.
+*/
+
+/*
+Exercício 8
+Temos um vetor de 5 posições, com o nome de 5 itens que nosso herói carrega na mochila. Quando o personagem chega numa porta, queremos detectar automaticamente se ele tem o não uma chave para deixá-lo passar. Crie uma função TemChave que percorre o vetor comparando se cada item tem o nome “chave” e retorne verdadeiro se encontrar alguma. Caso contrário, a função deve retornar falso.
+*/
+
+/*
+Exercício 9
+Precisamos limitar as posições em que um inimigo pode andar na tela. A menor posição possível é 0 e a maior, 100: qualquer valor maior que 100 ou menor que 0 sairia fora da tela. Precisamos de uma função LimitaPosicao que recebe uma POSICAO que pode ter qualquer valor positivo ou negativo, verifica se ela é válida ou não e retorna sempre um valor corrigido entre 0 e 100.
+*/
+
+/*
+Exercício 9
+Crie um game de jo-ken-po. A cada rodada, o jogador vê o menu:
+
+Escolha sua jogada: 1 - Papel 2 - Pedra 3 - Tesoura
+
+O jogo lê a opção do jogador e verifica se é válida. Se for inválida, o jogador perde a rodada e o jogo acaba. Se for válida, o computador escolhe uma resposta aleatória, que é mostrada ao jogador. Se o jogador ganhar, ele pode jogar mais uma rodada e sua pontuação aumenta. O jogo acaba quando o jogador perde uma rodada. A pontuação total é mostrada no fim do jogo.
+*/
