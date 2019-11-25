c35cc392c8116e10088d0193ba54c19d3bdbd9aa
Diego Borges <diego.ferreira@maxmilhas.com.br>
2019-11-25 15:06:33 -0300

    More exercises

---
+++ exercicio-2.js
@@ -0,0 +1,16 @@
+/*
+1 – Um funcionário de uma empresa recebe aumento salarial anualmente. Sabe-se que:
+• Esse funcionário foi contratado em 2010, com salário inicial de R$1000,00;
+• Em 2011 recebeu aumento de 1,5% sobre seu salário inicial;
+• A partir de 2012 (inclusive), os aumentos salariais sempre corresponderam ao dobro do percentual do ano anterior
+
+Faça um programa que determine o salário atual desse funcionário.
+*/
+
+/*
+2 – Faça um programa que calcule a soma dos primeiros 50 números pares.
+*/
+
+/*
+3 – Faça um programa onde o usuário possa escolher qual a tabuada que se deseja imprimir. Exiba a tabuada desse número de 1 a 10.
+*/
